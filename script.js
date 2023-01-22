153a74eb3949027543f00aa71957b3e3814911be
Caio <caiozs21@gmail.com>
2023-01-22 18:00:49 -0300

    chat+entrada na sala prontos

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+const autoScroll = msg =>{
+    document.querySelector('.a100').scrollIntoView();
+}
+
+const addReservadamente = (dest) =>{
+    const txtmsgPriv = document.querySelectorAll('.private_message .txtPriv');
+    const destMsg = document.querySelectorAll('.private_message .destinoMsg');
+    txtmsgPriv.forEach(texto =>{
+        texto.innerHTML+='reservadamente'
+    })
+    destMsg.forEach(msg =>{
+        if(nomeUsuario.name !== msg.innerHTML){
+            msg.parentNode.parentNode.classList.add('escondido')
+        }
+    })
+}
+let mainMsg= document.querySelector('main')
+
 const exibirMensagens = resposta =>{
     console.log('buscou certo as msgs')
-    console.log(resposta.data);
+    //console.log(resposta.data);
+    mainMsg.innerHTML = "";
+    let nome,paraQuem,textoMsg,typeMsg;
+    let idMsg=0
+    resposta.data.forEach(mensagemCriada => {
+        nome = mensagemCriada.from;
+        paraQuem = mensagemCriada.to;
+        textoMsg = mensagemCriada.text;
+        typeMsg = mensagemCriada.type;
+        idMsg++
+        const template = `<div class="mensagem ${typeMsg} a${idMsg}">
+        <div class="contMensagem">
+        <span class="hora">(09:22:28)</span>
+        <span class="nomeIdent">${nome}</span> <span class="txtPriv"></span> para 
+        <span class="nomeIdent destinoMsg">${paraQuem}</span>:
+        ${textoMsg}</div>
+    </div>`
+        mainMsg.innerHTML += template
+    });
+    addReservadamente(paraQuem);
+    autoScroll();
 }
 
 const erroNaBusca = erro =>{
@@ -9,11 +47,27 @@ const erroNaBusca = erro =>{
 
 const buscarMensagens= () =>{
 const promisseBuscaMsg=axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
 promisseBuscaMsg.then(exibirMensagens).catch(erroNaBusca)
+
+}
+
+const online = resposta =>{
+   // console.log('to online')
+}
+const offline = erro =>{
+    console.log('to off')
+}
+
+const verificarConec= () => {
+    const promisseConec = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeUsuario);
+    promisseConec.then(online).catch(offline)
 }
 
 const deuCerto = resposta =>{
     buscarMensagens()
+    setInterval(buscarMensagens , 3000)
+    setInterval(verificarConec , 5000)
 }
 
 const deuErrado = erro =>{  
@@ -22,15 +76,15 @@ const deuErrado = erro =>{
     perguntarNome()  
 }
 
-
+let nomeUsuario;
 const perguntarNome = () =>{
-    let nomeUsuario={name: prompt('Qual o seu nome?')};
+    nomeUsuario={name: prompt('Qual o seu nome?')};
     const promisseNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants' ,nomeUsuario);
     promisseNome.then(deuCerto);
     promisseNome.catch(deuErrado);
-
 }
 perguntarNome()
+
 
 
 
